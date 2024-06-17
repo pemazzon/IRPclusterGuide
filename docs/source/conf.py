648aab0821e3606a23de0f80d64b892619696831
Paolo Emilio Mazzon <p.mazzon@irpcds.org>
2024-06-17 11:53:28 +0200

    fix indice dei contenuti

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'IRPclusterGuide'
+copyright = '2024, IRP'
+author = 'P. E. Mazzon'
 
 release = '0.1'
 version = '0.1.0'
